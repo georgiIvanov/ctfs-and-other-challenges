9387bf7d42aee060c2cb2c77c93a63e14ac4837b
Georgi Ivanov <3618084+georgiIvanov@users.noreply.github.com>
2024-01-23 00:20:44 +0200

    Side entrance challenge

---
+++ test/damn-vulnerable-defi/SideEntranceTest.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@test/BaseTest.sol";
+import "@src/damn-vulnerable-defi/side-entrance/SideEntranceLenderPool.sol";
+
+contract SideEntranceTest is BaseTest {
+  
+  SideEntranceLenderPool pool;
+
+  uint256 constant ETHER_IN_POOL = 1_000 ether;
+  uint256 constant PLAYER_INITIAL_ETH_BALANCE = 1 ether;
+
+  function setUp() public override {
+    super.setUp();
+    vm.startPrank(deployer);
+    pool = new SideEntranceLenderPool();
+    pool.deposit{value: ETHER_IN_POOL}();
+    assertEq(address(pool).balance, ETHER_IN_POOL);
+
+    vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
+    assertEq(player.balance, PLAYER_INITIAL_ETH_BALANCE);
+  }
+
+  function testSideEntrance() public {
+    /** CODE YOUR SOLUTION HERE */
+    
+
+    /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+    // Player took all ETH from the pool
+    assertEq(address(pool).balance, 0);
+    assertEq(player.balance, ETHER_IN_POOL);
+  }
+}
