c91937b7c9181f96af2b6963825ec1b7e50b4fca
Georgi Ivanov <3618084+georgiIvanov@users.noreply.github.com>
2024-01-23 08:26:59 +0200

    Migrate to OZ v4.9.5

--- src/damn-vulnerable-defi/DamnValuableTokenSnapshot.sol
+++ src/damn-vulnerable-defi/DamnValuableTokenSnapshot.sol
@@ -1,25 +1,29 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 
 /**
  * @title DamnValuableTokenSnapshot
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
-contract DamnValuableTokenSnapshot is ERC20Votes {
+contract DamnValuableTokenSnapshot is ERC20Snapshot {
+    uint256 private _lastSnapshotId;
 
-    constructor(uint256 initialSupply) ERC20("DamnValuableToken", "DVT") EIP712("DamnValuableToken", "1") {
+    constructor(uint256 initialSupply) ERC20("DamnValuableToken", "DVT") {
         _mint(msg.sender, initialSupply);
     }
 
+    function snapshot() public returns (uint256 lastSnapshotId) {
+        lastSnapshotId = _snapshot();
+        _lastSnapshotId = lastSnapshotId;
+    }
+
     function getBalanceAtLastSnapshot(address account) external view returns (uint256) {
-        return _getVotingUnits(account);
+        return balanceOfAt(account, _lastSnapshotId);
     }
 
     function getTotalSupplyAtLastSnapshot() external view returns (uint256) {
-        return totalSupply();
+        return totalSupplyAt(_lastSnapshotId);
     }
 }
